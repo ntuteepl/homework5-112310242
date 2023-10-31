7ab9c3a2d414fa2df3dd6ffce52d4890807fa77a
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-31 15:04:26 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double f,i,c;
+    scanf("%lf %lf",&f,&i);
+    if(f<=60)
+        c=(double) f*i;
+    if(f>60&&f<=120)
+        c=(double) 60*i+(f-60)*i*1.33;
+    if(f>120)
+        c=(double) 60*i+60*i*1.33+(f-120)*i*1.66;
+    printf("%.1f\n",c);
+}
 
