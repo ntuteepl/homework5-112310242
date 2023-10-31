fda52fa3fa1a6582391cae688f6a4046d7e78ece
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-31 14:49:51 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float f,i;
+    scanf("%f",&f);
+    if(f<=800){
+        i=(float) f*9/10;
+        printf("%0.1f",i);
+        }
+    else if(f>800&&f<1500){
+        i=(float) (720+(f-800)*9/10)*9/10;
+        printf("%0.1f",i);
+        }
+    else if(f>=1500){
+        i=(float) 1287+(f-1500)*(9/10)*79/100;
+    printf("%0.1f",i);
+    }
+}
 
