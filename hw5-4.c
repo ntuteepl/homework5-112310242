32c85ad22735351daa7f123eb2cfe392f8715970
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-31 18:41:34 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -3,17 +3,13 @@
 
 int main()
 {
-    double h,m,a,b,t,c;
+    double h,m,a,b,c;
     scanf("%lf %lf",&h,&m);
     a=30*h+0.5*m;
     b=m*6;
-   // if(a<b){
-     //   t=a;
-       // a=b;
-        //b=t;
     c=a-b;
     if(c>180)
         c=360-c;
-   // }
     printf("%.3f\n",c);
 }
+
