531ddb243732d25acd7fd160e135607cc302c9b3
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-31 14:54:30 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -10,12 +10,11 @@ int main()
         printf("%0.1f",i);
         }
     else if(f>800&&f<1500){
-        i=(float) (720+(f-800)*9/10)*9/10;
+        i=(float) (f*9/10)*9/10;
         printf("%0.1f",i);
         }
     else if(f>=1500){
-        i=(float) 1287+(f-1500)*(9/10)*79/100;
+        i=(float) (f*9/10)*79/100;
     printf("%0.1f",i);
     }
 }
-
